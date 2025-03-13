364871feee4238109a2c6747e71ff11b9337527d
Amit <amit.dev1018@gmail.com>
2025-03-13 11:49:03 +0530

    add vehicle indent Page Design and driver Page Design in list menu

---
+++ RFQ.UI/Controllers/VehicleIndentController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RFQ.UI.Controllers
+{
+    public class VehicleIndentController : Controller
+    {
+        // GET: VehicleIndentController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: VehicleIndentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VehicleIndentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleIndentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VehicleIndentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VehicleIndentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VehicleIndentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VehicleIndentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
