d10190f27d055d84c86111184fc2d871fb984e2a
Amit <amit.dev1018@gmail.com>
2025-04-01 17:27:26 +0530

    update dynamic url in VehicleAdaptor

--- RFQ.UI.Infrastructure/Provider/VehicleAdaptor.cs
+++ RFQ.UI.Infrastructure/Provider/VehicleAdaptor.cs
@@ -28,7 +28,7 @@ namespace RFQ.UI.Infrastructure.Provider
             {
                 var _httpClient = new HttpClient();
                 _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _globalClass.Token);
-                var response = await _httpClient.GetAsync($"{_fleetLynkApiUrl}/VehicleType/GetAllVehicleCategory");
+                var response = await _httpClient.GetAsync(_fleetLynkApiUrl+ _config["Vehicle:GetAllVehicleCategory"]);
                 var responseData = await response.Content.ReadAsStringAsync();
                 var responseModel = JsonConvert.DeserializeObject<CommanResponseDto>(responseData);
                 if (responseModel != null)
@@ -84,7 +84,7 @@ namespace RFQ.UI.Infrastructure.Provider
             {
                 var _httpClient = new HttpClient();
                 _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _globalClass.Token);
-                var response = await _httpClient.GetAsync($"{_fleetLynkApiUrl}/VehicleType/GetAllVehicleType");
+                var response = await _httpClient.GetAsync(_fleetLynkApiUrl+ _config["Vehicle:GetAllVehicleType"]);
                 var responseData = await response.Content.ReadAsStringAsync();
                 var responseModel = JsonConvert.DeserializeObject<CommanResponseDto>(responseData);
                 if (responseModel != null)
@@ -106,7 +106,7 @@ namespace RFQ.UI.Infrastructure.Provider
             {
                 var _httpClient = new HttpClient();
                 _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _globalClass.Token);
-                var response = await _httpClient.GetAsync($"{_fleetLynkApiUrl}/VehicleType/GetAllOwnerOrVendor");
+                var response = await _httpClient.GetAsync(_fleetLynkApiUrl + _config["VehicleType:GetAllOwnerOrVendor"]);
                 var responseData = await response.Content.ReadAsStringAsync();
                 var responseModel = JsonConvert.DeserializeObject<CommanResponseDto>(responseData);
                 if (responseModel != null)
