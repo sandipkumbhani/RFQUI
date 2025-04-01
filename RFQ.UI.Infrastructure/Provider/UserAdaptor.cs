b2b415f776bf66f83ee2b96126e1ddc486f5d844
Amit <amit.dev1018@gmail.com>
2025-04-01 17:51:45 +0530

    set dynamic url in UserAdaptor

--- RFQ.UI.Infrastructure/Provider/UserAdaptor.cs
+++ RFQ.UI.Infrastructure/Provider/UserAdaptor.cs
@@ -23,7 +23,7 @@ namespace RFQ.UI.Infrastructure.Provider
             var _httpClient = new HttpClient();
 
             _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _globalClass.Token);
-            var response = await _httpClient.GetAsync($"{_fleetLynkApiUrl}/CompanyUser/GetUserAll");
+            var response = await _httpClient.GetAsync(_fleetLynkApiUrl + _config["User:GetUserAll"]);
             var responseData = await response.Content.ReadAsStringAsync();
             var responseModel = JsonConvert.DeserializeObject<CommanResponseDto>(responseData);
             if (responseModel != null)
