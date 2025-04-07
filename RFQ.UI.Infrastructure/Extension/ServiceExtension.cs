2985360633cc678de7a21c7225f50eb0fd5d4fb8
RuchitUkani <ruchitukani1501@gmail.com>
2025-04-07 12:50:11 +0530

    add crud operation in vendor form

--- RFQ.UI.Infrastructure/Extension/ServiceExtension.cs
+++ RFQ.UI.Infrastructure/Extension/ServiceExtension.cs
@@ -20,6 +20,7 @@ namespace RFQ.UI.Infrastructure.Extension
             services.AddScoped<LocationAdaptor>();
             services.AddScoped<IVehicleAdaptor,VehicleAdaptor>(); 
             services.AddScoped<FranchiseAdaptor>();
+            services.AddScoped<IVendorAdaptor,VendorAdaptor>();
             return services;
         }
     }
---
+++ RFQ.UI.Infrastructure/Provider/VendorAdaptor.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RFQ.UI.Domain.Interfaces;
+using RFQ.UI.Domain.Model;
+using RFQ.UI.Domain.RequestDto;
+using RFQ.UI.Domain.ResponseDto;
+using RFQ.UI.Models;
+using System.Text;
+
+namespace RFQ.UI.Infrastructure.Provider
+{
+    public class VendorAdaptor : IVendorAdaptor
+    {
+        private HttpClient _httpClient;
+        private readonly GlobalClass _globalClass;
+        private readonly IConfiguration _config;
+        private string _fleetLynkApiUrl;
+        public VendorAdaptor(HttpClient httpClient, GlobalClass globalClass, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _globalClass = globalClass;
+            _config = configuration;
+            _fleetLynkApiUrl = _config["ApiSettings:BaseUrl"];
+        }
+
+        public async Task<string> AddVendor(VendorRequestDto vendorRequestDto)
+        {
+            try
+            {
+                _httpClient = new HttpClient();
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _globalClass.Token);
+                var baseurl = _fleetLynkApiUrl + _config["Vendor:AddMasterParty"];
+                var vendor = JsonConvert.SerializeObject(vendorRequestDto);
+                var requestContent = new StringContent(vendor, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync(baseurl, requestContent);
+                var responseData = await response.Content.ReadAsStringAsync();
+                var responseModel = JsonConvert.DeserializeObject<CommanResponseDto>(responseData);
+                if (responseModel != null)
+                {
+                    var result = responseModel.StatusCode;
+                    if (result == 200)
+
+                        return "Vendor Saved";
+                    else
+                        return responseModel.ErrorMessage;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return string.Empty;
+        }
+
+        public async Task<string> DeleteVendor(int PartyId)
+        {
+            try
+            {
+                _httpClient = new HttpClient();
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _globalClass.Token);
+                var baseurl = _fleetLynkApiUrl + _config["Vendor:DeleteMasterParty"] + PartyId;
+                var response = await _httpClient.DeleteAsync(baseurl);
+                var responseData = await response.Content.ReadAsStringAsync();
+                var responseModel = JsonConvert.DeserializeObject<CommanResponseDto>(responseData);
+                if (responseModel != null)
+                {
+                    var result = responseModel.StatusCode;
+                    if (result == 200)
+                        return "Vendor Deleted";
+                    else
+                        return responseModel.ErrorMessage;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return "Failed to Delete Vendor";
+        }
+
+        public async Task<string> EditVendor(int PartyId, VendorRequestDto vendorRequestDto)
+        {
+            try
+            {
+                _httpClient = new HttpClient();
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _globalClass.Token);
+                var baseurl = _fleetLynkApiUrl + _config["Customer:UpdateMasterParty"] + PartyId;
+                var vendor = JsonConvert.SerializeObject(vendorRequestDto);
+                var requestContent = new StringContent(vendor, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PutAsync(baseurl, requestContent);
+                var responseData = await response.Content.ReadAsStringAsync();
+                var responseModel = JsonConvert.DeserializeObject<CommanResponseDto>(responseData);
+                if (responseModel != null)
+                {
+                    var result = responseModel.StatusCode;
+                    if (result == 200)
+                        return "Vendor Updated";
+                    else
+                        return responseModel.ErrorMessage;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return "Failed to update Vendor";
+        }
+
+        public async Task<IEnumerable<InternalMasterResponseDto>> GetAllInternalMaster()
+        {
+            try
+            {
+                var _httpClient = new HttpClient();
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _globalClass.Token);
+                var url = _fleetLynkApiUrl + _config["Vendor:GetAllInternalMaster"];
+                var response = await _httpClient.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {await response.Content.ReadAsStringAsync()}");
+                    return null;
+                }
+
+                var responseData = await response.Content.ReadAsStringAsync();
+                if (string.IsNullOrWhiteSpace(responseData))
+                {
+                    return null;
+                }
+                var responseModel = JsonConvert.DeserializeObject<CommanResponseDto>(responseData);
+                if (responseModel != null)
+                {
+                    var internalMasterList = JsonConvert.DeserializeObject<List<InternalMasterResponseDto>>(Convert.ToString(responseModel.Data!));
+                    return internalMasterList;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<IEnumerable<VendorResponseDto>> GetAllVendor()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _globalClass.Token);
+            var response = await _httpClient.GetAsync(_fleetLynkApiUrl + _config["Vendor:GetAllMasterParty"]);
+            var responseData = await response.Content.ReadAsStringAsync();
+            var responseModel = JsonConvert.DeserializeObject<CommanResponseDto>(responseData);
+            if (responseModel != null)
+            {
+                var vendorList = JsonConvert.DeserializeObject<List<VendorResponseDto>>(Convert.ToString(responseModel.Data!));
+                return vendorList;
+            }
+            return null;
+        }
+    }
+}
