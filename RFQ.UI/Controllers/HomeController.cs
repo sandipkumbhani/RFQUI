746cc97b73669391b942fa07b2da41d8a452a860
Pratik Sidd <pratik.dev540@gmail.com>
2025-05-07 12:47:17 +0530

    Quotanation to Customer

--- RFQ.UI/Controllers/HomeController.cs
+++ RFQ.UI/Controllers/HomeController.cs
@@ -50,6 +50,15 @@ namespace RFQ.UI.Controllers
         {
             return View();
         }
+        public IActionResult QuotationToCustomer()
+        {
+            return View("Views/Quotation/QuotationToCustomer.cshtml");
+        }
+        public IActionResult QuotationToAmendment()
+        {
+            return View("Views/Quotation/QuotationToAmendment.cshtml");
+        }
+
 
         [HttpPost]
         public async Task<IActionResult> UserSave([FromBody] UserRequestDto userRequestDto)
