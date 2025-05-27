ff0ff3be5d795451c719986abf5dfe404dc41203
Amit <amit.dev1018@gmail.com>
2025-05-27 18:19:30 +0530

    minor changes

--- RFQ.UI/Program.cs
+++ RFQ.UI/Program.cs
@@ -3,56 +3,50 @@ using RFQ.UI.Domain.Model;
 using RFQ.UI.Infrastructure.Extension;
 using RFQ.UI.MapperProfile;
 
-internal class Program
+var builder = WebApplication.CreateBuilder(args);
+var globalclass = new GlobalClass();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddEfcoreInfrastrucureService();
+builder.Services.AddApplicationService();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton(globalclass);
+builder.Services.AddAutoMapper(typeof(AutoMappersRegister));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        var globalclass = new GlobalClass();
-        // Add services to the container.
-        builder.Services.AddControllersWithViews();
-        builder.Services.AddEfcoreInfrastrucureService();
-        builder.Services.AddApplicationService();
-        builder.Services.AddHttpContextAccessor();
-        builder.Services.AddHttpClient();
-        builder.Services.AddSingleton(globalclass);
-        builder.Services.AddAutoMapper(typeof(AutoMappersRegister));
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-
-        app.Use(async (context, next) =>
-        {
-            // Read a specific cookie
-            var token = context.Request.Cookies["AuthToken"];
-
-            if (token != null)
-            {
-                globalclass.Token = token;
-            }
-            await next.Invoke();
-        });
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Login}/{action=Login}/{id?}");
-
-        app.Run();
-    }
+    app.UseDeveloperExceptionPage();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+
+app.Use(async (context, next) =>
+{
+    // Read a specific cookie
+    var token = context.Request.Cookies["AuthToken"];
+
+    if (token != null)
+    {
+        globalclass.Token = token;
+    }
+    await next.Invoke();
+});
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Login}/{action=Login}/{id?}");
+
+app.Run();
