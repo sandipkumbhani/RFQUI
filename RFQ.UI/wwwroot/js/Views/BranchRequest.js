8a6cc5472aa3eec2b090b52c0d99348e58ddefe9
Rucha <rucha.makwana1018@gmail.com>
2025-06-05 10:26:04 +0530

    branchRequest QuoteRate QutoeVendor Design Changes

--- RFQ.UI/wwwroot/js/Views/BranchRequest.js
+++ RFQ.UI/wwwroot/js/Views/BranchRequest.js
@@ -9,6 +9,7 @@
     GetAllCustomer();
     GetAllVehicleType();
     GetAllItemName();
+    GetAllPakingType();
 });
 function CheckValidation() {
     $("#ddlCustomerName").on("blur", function () {
@@ -110,7 +111,7 @@ function CheckValidation() {
     });
 
 }
-function OnSubmitCheckValidation() {
+function OnSubmitValidation() {
     if (!isValidateSelect($("#ddlCustomerName").val())) {
         toastr.warning("Please Select a Customer Name", "Validation Error");
         return false;
@@ -211,12 +212,14 @@ function GetAllCustomer() {
     });
 }
 function GetAllVehicleType() {
+    debugger;
     var getVehicleTypeUrl = '/Vehicle/GetAllMasterVehicleType'
     $.ajax({
         url: getVehicleTypeUrl,
         type: "GET",
         contentType: "application/json",
         success: function (response) {
+            debugger;
             const vehicleTypedropdown = document.getElementById("ddlVehicleType");
             let placeholderOption = document.createElement("option");
             placeholderOption.value = "";
@@ -263,4 +266,38 @@ function GetAllItemName() {
             toastr.error("Failed to Fetch Vehicle Type!", "Error");
         }
     });
+}
+function GetAllPakingType() {
+    var getUrl = '/CompanyMasterPackingType/GetAllMasterPackingType';
+    $.ajax({
+        url: getUrl,
+        type: "GET",
+        contentType: "application/json",
+        success: function (response) {
+            const vehicleTypedropdown = document.getElementById("ddlPackingType");
+            let placeholderOption = document.createElement("option");
+            placeholderOption.value = "";
+            placeholderOption.textContent = "Select a PakingType";
+            placeholderOption.disabled = true;
+            placeholderOption.selected = true;
+            vehicleTypedropdown.appendChild(placeholderOption);
+            response.forEach(item => {
+                const option = document.createElement("option");
+                option.value = item.packingId;
+                option.textContent = item.packingName;
+                vehicleTypedropdown.appendChild(option);
+            });
+            $('.selectpicker').selectpicker('refresh');
+        },
+        error: function (xhr, status, error) {
+            toastr.error("Failed to Fetch Paking Type!", "Error");
+        }
+    });
+}
+function Save() {
+
+    var isvalid = OnSubmitValidation();
+    if (!isvalid) {
+        return;
+    }
 }
