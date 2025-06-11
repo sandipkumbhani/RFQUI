6f0baeb5780673d989fe6fefd3aa8056533bbd7a
Amit <amit.dev1018@gmail.com>
2025-06-11 17:06:03 +0530

    update code for tableProduct make this code server side rendring

--- RFQ.UI.Domain/Interfaces/IProductAdaptor.cs
+++ RFQ.UI.Domain/Interfaces/IProductAdaptor.cs
@@ -1,11 +1,12 @@
-﻿using RFQ.UI.Domain.RequestDto;
+﻿using RFQ.UI.Domain.Helper;
+using RFQ.UI.Domain.RequestDto;
 using RFQ.UI.Domain.ResponseDto;
 
 namespace RFQ.UI.Domain.Interfaces
 {
     public interface IProductAdaptor
     {
-        Task<IEnumerable<ProductResponseDto>> GetAllProducts();
+        Task<PageList<ProductResponseDto>> GetAllProducts(PagingParam pagingParam);
         Task<string> AddProduct(ProductRequestDto productRequestDto);
         Task<string> EditProduct(int productId,ProductRequestDto productRequestDto);
         Task<string> DeleteProduct(int productId);
