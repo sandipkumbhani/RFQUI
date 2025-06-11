be58fe414059a3f3cfaa9a6f553912bef39632df
Amit <amit.dev1018@gmail.com>
2025-06-11 14:42:56 +0530

    Remove unnacesary code

--- RFQ.UI/wwwroot/js/common.js
+++ RFQ.UI/wwwroot/js/common.js
@@ -142,7 +142,6 @@ function ValidatePinCode(number) {
 }
 
 function FetchDataForTable(gridTableName, url) {
-    debugger;
     $('#tableDiv').show();
     $('#' + gridTableName + ' tbody').empty();
     $('#totalList').text('Total List: 0');
@@ -216,6 +215,7 @@ function GetGridHtml(response, gridTableName) {
                         <td>${item.whatsAppAuthKey}</td>
                         <td>${item.smtpHost}</td>
                         <td>${item.smtpPort}</td>
+                        <td>${item.smtpUsername}</td>
                         <td class="text-center action-items" style="cursor:pointer;">
                             <a class="icon-btn" onclick="EditCompanyConfiguration(${item.companyConfigId})"><i class="ri-edit-2-line"></i></a>
                             <a class="icon-btn" onclick="DeleteCompanyConfiguration(${item.companyConfigId})"><i class="ri-delete-bin-3-line"></i></a>
