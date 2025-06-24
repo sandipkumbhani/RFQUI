58dfb147fe34c3bbfbde5c81a3bcb3bbe0b3ebbf
Kishan101010 <kishan.patoliy1018@gmail.com>
2025-06-24 12:31:02 +0530

    Vendor  Rating Design change

--- RFQ.UI/wwwroot/js/Views/VendorRating.js
+++ RFQ.UI/wwwroot/js/Views/VendorRating.js
@@ -3,38 +3,61 @@ $(document).ready(function () {
 
     
 
-        const criteriaList = [
-            "Cost & Pricing", "Vehicle Conditions", "Vendor Communication",
-            "SLA / Delivery Efficiency", "Tracking Enabled Vehicles", "Statutory Compliance"
-        ];
+    const criteriaList = [
+        "Cost & Pricing", "Vehicle Conditions", "Vendor Communication",
+        "SLA / Delivery Efficiency", "Tracking Enabled Vehicles", "Statutory Compliance"
+    ];
 
-        criteriaList.forEach(function (criteria) {
-            const ratingName = "rating_" + criteria.replace(/\s|\/|&/g, '');
+    criteriaList.forEach(function (criteria) {
+        const ratingName = "rating_" + criteria.replace(/\s|\/|&/g, '');
 
-            let $tr = $('<tr></tr>');
-            $tr.append(`<td align="left">${criteria}</td>`);
+        let $tr = $('<tr></tr>');
+        $tr.append(`<td align="left">${criteria}</td>`);
 
-            let $tdStars = $('<td colspan="5"></td>');
-            let $ratingDiv = $('<div class="rating-stars text-center"></div>');
+        let $tdStars = $('<td colspan="5"></td>');
+        let $ratingDiv = $(`<div class="rating-stars text-center" data-rating-name="${ratingName}"></div>`);
 
-            for (let i = 5; i >= 1; i--) {
-                const id = `${ratingName}_${i}`;
-                const radio = `<input type="radio" name="${ratingName}" id="${id}" value="${i}" />`;
-                const label = `<label for="${id}"><i class="fa fa-star text-center"></i></label>`;
-                $ratingDiv.append(radio).append(label);
-            }
+        for (let i = 1; i <= 5; i++) {
+            const id = `${ratingName}_${i}`;
+            const radio = `<input type="radio" name="${ratingName}" id="${id}" value="${i}" style="display:none;" />`;
+            const label = `<label data-index="${i}" for="${id}" style="font-size: 24px; color: #ccc; cursor: pointer;">★</label>`;
+            $ratingDiv.append(radio).append(label);
+        }
 
-            $tdStars.append($ratingDiv);
-            $tr.append($tdStars);
+        $tdStars.append($ratingDiv);
+        $tr.append($tdStars);
 
-            const remarksInput = `<td><input type="text" class="form-control" placeholder="Remarks" /></td>`;
-            $tr.append(remarksInput);
+        const remarksInput = `<td><input type="text" class="form-control" placeholder="Remarks" /></td>`;
+        $tr.append(remarksInput);
 
-            $('#tableDiv').append($tr);
-        });
+        $('#tableDiv').append($tr);
+    });
         GetAllPakingType();
         GetAllInternalMaster();
+});
+
+$(document).on('click', '.rating-stars label', function () {
+    const $label = $(this);
+    const $ratingDiv = $label.closest('.rating-stars');
+    const ratingName = $ratingDiv.data('rating-name');
+    const selectedIndex = parseInt($label.attr('data-index'));
+
+    // Reset all stars
+    $ratingDiv.find('label').each(function () {
+        $(this).css('color', '#ccc');
     });
+
+    // Highlight selected stars
+    $ratingDiv.find('label').each(function () {
+        if (parseInt($(this).attr('data-index')) <= selectedIndex) {
+            $(this).css('color', 'gold');
+        }
+    });
+
+    // Check the corresponding radio button
+    $ratingDiv.find(`input#${ratingName}_${selectedIndex}`).prop('checked', true);
+});
+
 
 function GetAllPakingType() {
     var getUrl = '/CompanyMasterPackingType/GetAllMasterPackingType';
