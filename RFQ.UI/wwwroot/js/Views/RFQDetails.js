7319696e328a9fa529d2fc7f848fbcafdbe75065
Kishan101010 <kishan.patoliy1018@gmail.com>
2025-07-15 11:18:28 +0530

    Validation Add In Rfq Detail

--- RFQ.UI/wwwroot/js/Views/RFQDetails.js
+++ RFQ.UI/wwwroot/js/Views/RFQDetails.js
@@ -58,14 +58,86 @@ function CheckValidation() {
     });
 }
 function OnSubmitCheckValidation() {
-    debugger;
     if (!isValidateSelect($("#ddlLocation").val())) {
         toastr.warning("Please Select a Location", "Validation Error");
         return false;
     }
+    if (IsNullOrEmpty($("#txtRfqNo").val())) {
+        toastr.warning("Please enter a RFQ No", "Validation Error");
+        return false;
+    }
+    if (IsNullOrEmpty($("#txtRfqDate").val())) {
+        toastr.warning("Please enter a RFQ Date", "Validation Error");
+        return false;
+    }
+    if (IsNullOrEmpty($("#txtRfqExpiredOn").val())) {
+        toastr.warning("Please enter a RFQ Expired On", "Validation Error");
+        return false;
+    }
+    if (!isValidateSelect($("#ddlIndent").val())) {
+        toastr.warning("Please Select a Indent No", "Validation Error");
+        return false;
+    }
+    if (!isValidateSelect($("#ddlCustomerName").val())) {
+        toastr.warning("Please Select a Customer Name", "Validation Error");
+        return false;
+    }
+    if (IsNullOrEmpty($("#txtVehicleReqDate").val())) {
+        toastr.warning("Please enter a Vehicle Req On", "Validation Error");
+        return false;
+    }
+    if (IsNullOrEmpty($("#txtOrigin").val())) {
+        toastr.warning("Please enter a Origin/From", "Validation Error");
+        return false;
+    }
+    if (IsNullOrEmpty($("#txtDestination").val())) {
+        toastr.warning("Please enter a Destination/To", "Validation Error");
+        return false;
+    }
+    if (!isValidateSelect($("#ddlVehicleType").val())) {
+        toastr.warning("Please Select a Vehicle Type", "Validation Error");
+        return false;
+    }
+    if (IsNullOrEmpty($("#txtNoofVehicles").val())) {
+        toastr.warning("Please enter a No. of Vehicles", "Validation Error");
+        return false;
+    }   
+    if (IsNullOrEmpty($("#txtMaxCosting").val())) {
+        toastr.warning("Please enter a Max Costing", "Validation Error");
+        return false;
+    }   
+    if (IsNullOrEmpty($("#txtPerDay").val())) {
+        toastr.warning("Please enter a Detention Per Day", "Validation Error");
+        return false;
+    }   
+    if (IsNullOrEmpty($("#txtFreeDay").val())) {
+        toastr.warning("Please enter a Detention Free Days", "Validation Error");
+        return false;
+    }   
+    if (!isValidateSelect($("#ddlRfqPriority").val())) {
+        toastr.warning("Please Select a RFQ Priority", "Validation Error");
+        return false;
+    }
+    if (!isValidateSelect($("#ddlRfqType").val())) {
+        toastr.warning("Please Select a RFQ Type", "Validation Error");
+        return false;
+    }
+    if (!isValidateSelect($("#ddlItemName").val())) {
+        toastr.warning("Please Select a Item Name", "Validation Error");
+        return false;
+    }
+    if (!isValidateSelect($("#ddlPackingType").val())) {
+        toastr.warning("Please Select a Packing Type", "Validation Error");
+        return false;
+    }
+    if (IsNullOrEmpty($("#txtSpecialInstructions").val())) {
+        toastr.warning("Please enter a Special Instructions", "Validation Error");
+        return false;
+    }   
+
+
     return true;
 }
-
 function GetAllCustomer() {
     var GetUrl = '/Customer/GetDrpCustomerList';
     $.ajax({
