fe253794de6cd4dd5e1a054efcb0558ed061ed70
Amit <amit.dev1018@gmail.com>
2025-07-29 19:24:47 +0530

    Creat and update Api AddRfqLinkData

---
+++ RFQ.UI.Application/Provider/RfqLinkService.cs
@@ -0,0 +1,19 @@
+﻿using RFQ.UI.Application.Interface;
+using RFQ.UI.Domain.Interfaces;
+using RFQ.UI.Domain.RequestDto;
+
+namespace RFQ.UI.Application.Provider
+{
+   public class RfqLinkService : IRfqLinkService
+    {
+        private readonly IRfqLinkAdaptor _rfqLinkAdaptor;
+        public RfqLinkService(IRfqLinkAdaptor rfqLinkAdaptor)
+        {
+            _rfqLinkAdaptor  = rfqLinkAdaptor;
+        }
+        public Task<bool> AddRfqLinkData(RfqLinkRequestDto rfqLinkRequestDto)
+        {
+            return _rfqLinkAdaptor.AddRfqLinkData(rfqLinkRequestDto);
+        }
+    }
+}
---
+++ RFQ.UI.Domain/RequestDto/RfqLinkRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RFQ.UI.Domain.RequestDto
+{
+    public class RfqLinkRequestDto
+    {
+        public int RfqRateLinkId { get; set; }
+        public int RfqId { get; set; }
+        public int VendorId { get; set; }
+        public string SharedLink { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
