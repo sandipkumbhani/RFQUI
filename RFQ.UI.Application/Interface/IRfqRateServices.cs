0af81f31c3006064cd18e8784375de471fa0a027
Kishan101010 <kishan.patoliy1018@gmail.com>
2025-07-29 14:35:04 +0530

    Create RfqRate Controller

---
+++ RFQ.UI.Application/Interface/IRfqRateServices.cs
@@ -0,0 +1,14 @@
+﻿using RFQ.UI.Domain.RequestDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFQ.UI.Application.Interface
+{
+    public interface IRfqRateServices
+    {
+        Task<RfqRateRequestDto> AddRfqRate(RfqRateRequestDto rfqRateRequestDto);
+    }
+}
