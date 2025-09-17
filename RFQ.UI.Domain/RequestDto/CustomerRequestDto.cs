4ed75c5b277d1f0507a2c4d2f54216e3df85043f
Kishan101010 <kishan.patoliy1018@gmail.com>
2025-09-17 18:52:22 +0530

    Dto Chnages

--- RFQ.UI.Domain/RequestDto/CustomerRequestDto.cs
+++ RFQ.UI.Domain/RequestDto/CustomerRequestDto.cs
@@ -4,7 +4,7 @@
     {
         public int PartyId { get; set; }
 
-        public int CompanyId { get; set; }
+        public int? CompanyId { get; set; }
 
         public int PartyTypeId { get; set; } = 6;
 
