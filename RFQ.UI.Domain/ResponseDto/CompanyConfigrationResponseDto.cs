f68a573733c13dbf03f102380477c39e921a6a4a
Kishan101010 <kishan.patoliy1018@gmail.com>
2025-10-30 11:45:49 +0530

    CompanyConfigurationResponseDto change

--- RFQ.UI.Domain/ResponseDto/CompanyConfigrationResponseDto.cs
+++ RFQ.UI.Domain/ResponseDto/CompanyConfigrationResponseDto.cs
@@ -3,25 +3,17 @@
     public class CompanyConfigurationResponseDto
     {
         public int CompanyConfigId { get; set; }
-
         public int CompanyId { get; set; }
-
         public string CompanyName { get; set; }
-
         public string? SMSProvider { get; set; }
-
         public string SMSAuthKey { get; set; }
-
         public string? WhatsAppProvider { get; set; }
-
         public string WhatsAppAuthKey { get; set; }
-
         public string? SMTPHost { get; set; }
-
         public int SMTPPort { get; set; }
-
         public string? SMTPUsername { get; set; }
-
         public string? SMTPPassword { get; set; }
+        public int StatusId { get; set; }
+        public string? IsActive { get; set; }
     }
 }
