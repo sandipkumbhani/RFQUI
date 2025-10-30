3ec37c6cf2a2a7a134435dcbcc810a23b9132104
Amit <amit.dev1018@gmail.com>
2025-10-30 11:37:29 +0530

    Add Field StatusId Company Config table

--- RFQ.UI.Domain/RequestDto/CompanyConfigrationRequestDto.cs
+++ RFQ.UI.Domain/RequestDto/CompanyConfigrationRequestDto.cs
@@ -3,23 +3,15 @@
     public class CompanyConfigrationRequestDto
     {
         public int CompanyConfigId { get; set; }
-
         public int CompanyId { get; set; }
-
         public string? SMSProvider { get; set; }
-
         public string? SMSAuthKey { get; set; }
-
         public string? WhatsAppProvider { get; set; }
-
         public string? WhatsAppAuthKey { get; set; }
-
         public string? SMTPHost { get; set; }
-
         public int SMTPPort { get; set; }
-
         public string? SMTPUsername { get; set; }
-
         public string? SMTPPassword { get; set; }
+        public int StatusId { get; set; }
     }
 }
--- RFQ.UI/Controllers/CompanyConfigurationController.cs
+++ RFQ.UI/Controllers/CompanyConfigurationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RFQ.UI.Application.Interface;
+using RFQ.UI.Domain.Enum;
 using RFQ.UI.Domain.Model;
 using RFQ.UI.Domain.RequestDto;
 using RFQ.UI.Extension;
@@ -92,6 +93,7 @@ namespace RFQ.UI.Controllers
             {
                 if (companyConfigrationRequestDto != null)
                 {
+                    companyConfigrationRequestDto.StatusId = (int)EStatus.IsActive;
                     var result = await _companyConfigurationServices.AddCompanyConfiguration(companyConfigrationRequestDto);
                     return Json(new { result = "Success" });
                 }
