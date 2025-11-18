6e8eeba4fa12c61913774f0adc2a93e06c268af5
Amit <amit.dev1018@gmail.com>
2025-11-18 18:19:04 +0530

    Remover Driver Photos From server and give uniq Name

--- RFQ.UI/Controllers/DriverController.cs
+++ RFQ.UI/Controllers/DriverController.cs
@@ -66,7 +66,7 @@ namespace RFQ.UI.Controllers
                     {
                         Directory.CreateDirectory(uploadsFolder);
                     }
-                    uniqueFileName = DateTime.Now.ToString("dd-MM-yyyy") + "_" + file.FileName;
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + DateTime.Now.ToString("MM/dd/yyyy") + "_" + file.FileName;
                     string filePath = Path.Combine(uploadsFolder, uniqueFileName);
                     using (var fileStream = new FileStream(filePath, FileMode.Create))
                     {
