bd321dfea2b8c39b58d73262f6867b909af17e40
Amit <amit.dev1018@gmail.com>
2026-03-16 11:54:41 +0530

    Add Loggr into WhatsAppService

--- RFQ.UI.Application/Provider/WhatsAppService.cs
+++ RFQ.UI.Application/Provider/WhatsAppService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using RFQ.UI.Application.Interface;
 using RFQ.UI.Domain.Model;
@@ -9,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
 namespace RFQ.UI.Application.Provider
@@ -18,18 +20,19 @@ namespace RFQ.UI.Application.Provider
         private readonly IConfiguration _config;
         private readonly HttpClient _httpClient;
         private readonly AppSettingsGlobal _appSettings;
-        public WhatsAppService(IConfiguration configuration, HttpClient httpClient, AppSettingsGlobal appSettings)
+        private readonly ILogger<WhatsAppService> _logger;
+        public WhatsAppService(IConfiguration configuration, HttpClient httpClient, AppSettingsGlobal appSettings, ILogger<WhatsAppService> logger)
         {
             _config = configuration;
             _httpClient = httpClient;
             _appSettings = appSettings;
+            _logger = logger;
         }
 
         public async Task<string> SendMessageAsync(WhatsAppRequestDto requestDto)
         {
             try
             {
-
                 var url = _appSettings.WhatsAppApiUrl;
                 var whatsAppConfig = _config.GetSection("WhatsAppService");
 
@@ -43,13 +46,17 @@ namespace RFQ.UI.Application.Provider
                     VendorId = Guid.Parse(whatsAppConfig["VendorId"] ?? Guid.Empty.ToString())
                 };
 
-                var content = new StringContent(JsonConvert.SerializeObject(body),Encoding.UTF8,"application/json");
+                _logger.LogInformation("WhatsApp API URL: {Url}", url);
+                _logger.LogInformation("WhatsApp Request content: {RequestBody}", JsonConvert.SerializeObject(body));
+                var content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
                 var response = await _httpClient.PostAsync(url, content);
                 var result = await response.Content.ReadAsStringAsync();
+                _logger.LogInformation("WhatsApp Api response: {response}", result);
                 return result;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                _logger.LogError(ex, "Error occurred while sending WhatsApp message to {MobileNo}", requestDto.MobileNo);
                 throw;
             }
         }
