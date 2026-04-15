ea4ca9a051ff0158222f4942158ce7e276ec31e7
Pratik-Sidd <pratik.dev540@gmail.com>
2026-04-15 13:29:04 +0530

    Changes for dashboard card design.

--- RFQ.UI.Domain/ResponseDto/DashboardCardResponseDto.cs
+++ RFQ.UI.Domain/ResponseDto/DashboardCardResponseDto.cs
@@ -5,6 +5,10 @@
         public int RecordCount { get; set; }
         public string CardHeader { get; set; }
         public string CardId { get; set; }
-
+        public string Icon { get; set; }
+        public string Color { get; set; }
+        public string BgColor { get; set; }
+        public string BorderColor { get; set; }
+        public string Trend { get; set; }
     }
 }
